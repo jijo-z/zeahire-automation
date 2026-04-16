4f0ad0dd48dcd567abf292a7a767b584cec06df2
jijo <jijo@zealogics.com>
2026-04-16 14:37:52 +0530

    email trigger fix

--- run.py
+++ run.py
@@ -1,39 +1,35 @@
-import os
-import subprocess
 import smtplib
+import os
 from email.mime.text import MIMEText
 
-# Run tests
-result = subprocess.run(["pytest", "-v"], capture_output=True, text=True)
+def send_email():
+    sender = os.getenv("EMAIL_USER")
+    password = os.getenv("EMAIL_PASS")
+    receivers = os.getenv("EMAIL_RECEIVERS").split(",")
 
-# Email content
-subject = "Automation Test Result"
-body = f"""
-Test Execution Completed
+    subject = "ZeaHire Automation Report"
 
-STATUS: {"PASS" if result.returncode == 0 else "FAIL"}
+    report_url = "https://jijo-z.github.io/zeahire-automation/"
 
-OUTPUT:
-{result.stdout}
+    body = f"""
+Automation Execution Completed
+
+View Full Allure Report:
+{report_url}
+
+Regards,
+Automation Bot
 """
 
-msg = MIMEText(body)
-msg["Subject"] = subject
-msg["From"] = os.getenv("EMAIL_USER")
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = ", ".join(receivers)
 
-# 👉 Add BOTH emails here
-recipients = [
-    os.getenv("EMAIL_USER"),
-    "secondperson@gmail.com"
-]
-
-msg["To"] = ", ".join(recipients)
-
-# Send email
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-    server.login(os.getenv("EMAIL_USER"), os.getenv("EMAIL_PASS"))
-    server.sendmail(
-        os.getenv("EMAIL_USER"),
-        recipients,
-        msg.as_string()
-    )
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(sender, password)
+        server.sendmail(sender, receivers, msg.as_string())
+    
+if __name__ == "__main__":
+    send_email()        
