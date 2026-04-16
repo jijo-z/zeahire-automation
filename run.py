8735371d2ce0ca1f3c3077a142460df811fc1b00
jijo <jijo@zealogics.com>
2026-04-16 14:57:05 +0530

    email integration final

--- run.py
+++ run.py
@@ -1,28 +1,33 @@
-import smtplib
 import os
+import subprocess
+import smtplib
 from email.mime.text import MIMEText
+
+
+def run_tests():
+    subprocess.run([
+        "pytest",
+        "-m", "smoke",
+        "--alluredir=reports/allure-results"
+    ])
+
 
 def send_email():
     sender = os.getenv("EMAIL_USER")
     password = os.getenv("EMAIL_PASS")
     receivers = os.getenv("EMAIL_RECEIVERS").split(",")
 
-    subject = "ZeaHire Automation Report"
-
     report_url = "https://jijo-z.github.io/zeahire-automation/"
 
     body = f"""
 Automation Execution Completed
 
-View Full Allure Report:
+View Report:
 {report_url}
-
-Regards,
-Automation Bot
 """
 
     msg = MIMEText(body)
-    msg["Subject"] = subject
+    msg["Subject"] = "Automation Report"
     msg["From"] = sender
     msg["To"] = ", ".join(receivers)
 
@@ -30,6 +35,9 @@ Automation Bot
         server.starttls()
         server.login(sender, password)
         server.sendmail(sender, receivers, msg.as_string())
-    
+
+
+# 🚀 MAIN FLOW
 if __name__ == "__main__":
-    send_email()        
+    run_tests()
+    send_email()
